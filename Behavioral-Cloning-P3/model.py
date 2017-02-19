f81936ab216dad4d4ee8aaf39b63e798446bcf3e
fujiki <abcdef.nakamura@gmail.com>
2017-02-19 14:15:36 +0900

    train the model with center, left and right images - using generator

--- Behavioral-Cloning-P3/model.py
+++ Behavioral-Cloning-P3/model.py
@@ -5,6 +5,7 @@ import cv2
 
 import numpy as np
 import pandas as pd
+import sklearn
 from sklearn.model_selection import train_test_split
 
 import keras.backend as K
@@ -13,20 +14,49 @@ from keras.callbacks import EarlyStopping
 import model_nvidia_like
 
 
-def load_data():
-    # TODO: use the left and right images
-    df_drive_log = pd.read_csv('./data/driving_log.csv')
-    y = df_drive_log['steering'].values
-    X = []
-    images = df_drive_log['center'].values
-    for img in images:
-        img_file = './data/' + img
-        img = cv2.imread(img_file)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        X.append(img)
-    X = np.array(X)
+# path to the data directory
+data_dir = './data/'
+# Load the driving log data
+df_drive_log = pd.read_csv(data_dir + 'driving_log.csv')
 
-    return X, y
+
+def generator(df_drive_log, batch_size=128):
+    nb_samples = df_drive_log.shape[0]
+    while True:
+        sklearn.utils.shuffle(df_drive_log)
+
+        for offset in range(0, nb_samples, batch_size):
+            df_drive_log_batches = df_drive_log[offset:offset + batch_size]
+
+            images = []
+            steerings = []
+
+            for index, row in df_drive_log_batches.iterrows():
+                correction = 0.2
+                # center image
+                img_file_center = data_dir + row['center'].strip()
+                img_center = cv2.imread(img_file_center)
+                img_center = cv2.cvtColor(img_center, cv2.COLOR_BGR2RGB)
+                # left image
+                img_file_left = data_dir + row['left'].strip()
+                img_left = cv2.imread(img_file_left)
+                img_left = cv2.cvtColor(img_left, cv2.COLOR_BGR2RGB)
+                # right image
+                img_file_right = data_dir + row['right'].strip()
+                img_right = cv2.imread(img_file_right)
+                img_right = cv2.cvtColor(img_right, cv2.COLOR_BGR2RGB)
+                # steering
+                steering_center = row['steering']
+                steering_left = steering_center + correction
+                steering_right = steering_center - correction
+
+                images.extend([img_center, img_left, img_right])
+                steerings.extend([steering_center, steering_left, steering_right])
+
+            X = np.array(images)
+            y = np.array(steerings)
+
+            yield sklearn.utils.shuffle(X, y)
 
 
 def flip(image):
@@ -37,26 +67,28 @@ def flip(image):
 
 
 def main():
-    # Prepare the data
-    t0 = time.time()
-    X, y = load_data()
-    print('Loading time = {} seconds'.format(time.time() - t0))
-    X_train, X_test, y_train, y_test = \
-        train_test_split(X, y, test_size=0.25, random_state=0)
-
-    # Train the model
     batch_size = 128
-    model = model_nvidia_like.get_model()
-    early_stopping = EarlyStopping(monitor='val_loss', patience=5)
-    model.fit(
-        X_train, y_train,
-        batch_size=batch_size, nb_epoch=10,
-        callbacks=[early_stopping],
-        validation_split=0.25,
-        )
 
-    # Test the model
-    model.evaluate(X_test, y_test, batch_size=batch_size)
+    # Prepare the data
+    # train val split
+    df_drive_log_train, df_drive_log_val = \
+        train_test_split(df_drive_log, test_size=0.25, random_state=0)
+
+    # create the model (the model is defined in another script)
+    model = model_nvidia_like.get_model()
+    # generators
+    train_generator = generator(df_drive_log_train, batch_size=batch_size)
+    val_generator = generator(df_drive_log_val, batch_size=batch_size)
+    # callbacks
+    early_stopping = EarlyStopping(monitor='val_loss', patience=5)
+    # Train the model
+    model.fit_generator(
+        generator=train_generator,
+        samples_per_epoch=df_drive_log_train.shape[0],
+        nb_epoch=10,
+        validation_data=val_generator,
+        nb_val_samples=df_drive_log_val.shape[0]
+    )
 
     # Save the model
     model.save('model.h5')
