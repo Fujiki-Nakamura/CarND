6b8098d26fc789d88ca8ba175f58ba9798776868
fujiki <abcdef.nakamura@gmail.com>
2017-03-21 18:29:59 +0900

    Add some directries

---
+++ CarND-Transfer-Learning-Lab/run_inception.sh
@@ -0,0 +1,3 @@
+python feature_extraction.py \
+--training_file ./data/inception_cifar10_100_bottleneck_features_train.p \
+--validation_file ./data/inception_cifar10_bottleneck_features_validation.p
